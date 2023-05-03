61c3479bf868b032f320772523aca07bcb9fe9c5
Tobi <39420707+TobidieTopfpflanze@users.noreply.github.com>
2023-05-03 10:30:56 +0200

    Update shapes.py

--- LED_matrix/shapes.py
+++ LED_matrix/shapes.py
@@ -5,9 +5,10 @@ from luma.led_matrix.device import max7219
 serial = spi(port=0, device=0, gpio=noop())
 device = max7219(serial, cascaded=4, block_orientation=90, rotate=0, blocks_arranged_in_reverse_order=True)
 
-with canvas(device) as draw:
-    # Draw point
-    draw.point((1, 1), fill="white")
-
-    # Draw bar player 2
-    draw.line((5, 1, 5, 3), fill="white")
+while True: 
+    with canvas(device) as draw:
+        # Draw point
+        draw.point((1, 1), fill="white")
+    
+        # Draw bar player 2
+        draw.line((5, 1, 5, 3), fill="white")
